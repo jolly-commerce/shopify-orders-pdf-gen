c88d8c2e18bf1fc10aad6dcf5a16ec91ec880a4c
Kevin Miguet <kevin.miguet@gmx.fr>
2023-01-27 15:23:04 +0300

    function

--- netlify/functions/generate-pdf/generate-pdf.ts
+++ netlify/functions/generate-pdf/generate-pdf.ts
@@ -1,12 +1,35 @@
 import { Handler } from '@netlify/functions'
+import pdfkit from 'pdfkit';
 
 export const handler: Handler = async (event, context) => {
-  const { name = 'stranger' } = event.queryStringParameters
+ // Get data from the request body
+ if (!event.body) {
+ return {
+    statusCode: 400,
+  };
+ }
+ const data = JSON.parse(event.body);
 
-  return {
-    statusCode: 200,
-    body: JSON.stringify({
-      message: `Hello, ${name}!`,
-    }),
-  }
+ // Create a PDF document
+ const doc = new pdfkit();
+ doc.pipe(data);
+
+ // Add addresses to the PDF
+ data.forEach((row) => {
+   doc.text(`${row.Name}\n`);
+   doc.text(`${row["Shipping Street"]}\n`);
+   doc.text(`${row["Shipping City"]}, ${row["Shipping Zip"]}\n`);
+   doc.text(`${row["Shipping Province"]}, ${row["Shipping Country"]}\n`);
+   doc.addPage();
+ });
+
+ // End the PDF and send it to the client
+ doc.end();
+ return {
+   statusCode: 200,
+   headers: {
+     'Content-Type': 'application/pdf',
+   },
+   body: doc,
+ };
 }
