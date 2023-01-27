69501ff2bf0d5eb8e585f3254555630f219ce7a0
Kevin Miguet <kevin.miguet@gmx.fr>
2023-01-27 15:42:16 +0300

    f

--- netlify/functions/generate-pdf/generate-pdf.ts
+++ netlify/functions/generate-pdf/generate-pdf.ts
@@ -14,14 +14,31 @@ export const handler: Handler = async (event, context) => {
  const doc = new pdfkit();
  doc.pipe(data);
 
- // Add addresses to the PDF
- data.forEach((row) => {
-   doc.text(`${row.Name}\n`);
-   doc.text(`${row["Shipping Street"]}\n`);
-   doc.text(`${row["Shipping City"]}, ${row["Shipping Zip"]}\n`);
-   doc.text(`${row["Shipping Province"]}, ${row["Shipping Country"]}\n`);
-   doc.addPage();
- });
+
+ // define the number of addresses per row
+ const addressesPerRow = 3;
+ const colSize = 300;
+ const rowSize = 200;
+
+ // set the font size and font family
+ doc.fontSize(12).font('Helvetica');
+
+ // loop through the addresses and add them to the PDF
+ for (let i = 0; i < data.length; i++) {
+    const row = data[i]
+    const address = `${row.Name}\n
+    ${row["Shipping Street"]}\n
+    ${row["Shipping City"]}, ${row["Shipping Zip"]}\n
+    ${row["Shipping Province"]}, ${row["Shipping Country"]}\n
+    `
+   // calculate the x and y coordinates for the current address
+   const x = (i % addressesPerRow) * colSize;
+   const y = Math.floor(i / addressesPerRow) * rowSize;
+
+   // add the address to the PDF at the specified coordinates
+   doc.text(address, x, y);
+ }
+
 
  // End the PDF and send it to the client
  doc.end();
